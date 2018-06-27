f4f0c187e137f76bc295237ba6a12345eabb7ef8
spiskommg <spisko@maymediagroup.com>
2018-06-26 23:35:59 -0400

    added folder name to dist fodler reference

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const forceSSL = function() {
 }
 // ForceSSL middleware
 app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/pisko-task'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/pisko-task/index.html'));
 });
