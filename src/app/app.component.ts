9fbfeb1d59c45f6b84d3fed91f9de4c191662741
spiskommg <spisko@maymediagroup.com>
2018-06-26 21:54:33 -0400

    built out the task test for kern ferry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  time = 0;
+  showMondrian = false;
+  showSelector = false;
+  showLoading = false;
+  selectedCompany: object;
+
+  companies = [
+    { name: 'Novartis' },
+    { name: 'Walmart' },
+    { name: 'Wegmans' }
+  ];
+
+  data = [
+    {
+      title: 'Manage Users',
+      path: '/manage/users',
+      icon: 'assets/manage users.svg',
+    },
+    {
+      title: 'Orders & Subscriptions',
+      path: '/manage/orders',
+      icon: 'assets/orders and subscriptions.svg',
+    },
+    {
+      title: 'Data Upload',
+      path: '/manage/data',
+      icon: 'assets/data upload.svg',
+    },
+    {
+      title: 'Module Settings',
+      path: '/manage/orders',
+      icon: 'assets/module settings.svg',
+    },
+    {
+      title: 'Organization Content',
+      path: '/manage/content',
+      icon: 'assets/organization content.svg',
+    },
+    {
+      title: 'Content Library Manager',
+      path: '/manage/library',
+      icon: 'assets/content library.svg',
+    },
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    let timeoutId = setInterval(() => {
+      console.log(this.time);
+      switch(this.time){
+        case 0:
+          this.showLoading = true;
+          break;
+        case 2:
+          this.showLoading = false;
+          this.showSelector = true;
+          break;
+        case 3:
+          this.showMondrian = true;
+          break;
+      }
+      this.time++;
+    }, 1000);
+  }
+
+  onSelectCompany (event) {
+    console.log('from parent', event);
+    this.showMondrian = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
+import { MondrianComponent } from './mondrian/mondrian.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockComponent,
+    MondrianComponent,
+    SelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
